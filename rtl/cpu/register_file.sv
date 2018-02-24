2c112387b057d0e17324462b48b86910992d28f8
ferris <yupferris@gmail.com>
2018-02-24 19:01:30 +0100

    Make sure r0 is 0
    This is done by making two changes:
    1. Make register file regs array only go down to index 1
    2. Add addr compares to the read_data paths, so that if the addr is zero, the value should be zero as well, else read from the array.
    
    Together this appears to add a small amount of logic driving the WE line for the inferred RAM, as well as a 32-bit mux on each read port. This isn't ideal, but is still pretty self-contained, and I didn't feel comfortable letting the RAM contents be unitialized or relying on the output ports of the inferred RAM to behave properly when the read addr was zero. It's a bit of a shame that the inferred RAM doesn't have a proper clear to zero line that we could use on reset, but this alternative isn't terrible.

--- rtl/cpu/register_file.sv
+++ rtl/cpu/register_file.sv
@@ -14,14 +14,14 @@ module register_file(
     input [4:0] write_addr,
     input [31:0] write_data);
 
-    logic [31:0] regs[0:31];
+    logic [31:0] regs[1:31];
 
-    assign read_data1 = regs[read_addr1];
-    assign read_data2 = regs[read_addr2];
+    assign read_data1 = read_addr1 != 0 ? regs[read_addr1] : 32'h0;
+    assign read_data2 = read_addr2 != 0 ? regs[read_addr2] : 32'h0;
 
     always_ff @(posedge clk or negedge reset_n) begin
         if (!reset_n) begin
-            // TODO: reg0 <= 0 somehow
+            // Do nothing
         end
         else begin
             if (write_enable && write_addr != 0) begin
