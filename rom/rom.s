aa73cf929db36fcb942e3321336171aa6b3e06c1
ferris <yupferris@gmail.com>
2018-02-28 00:34:27 +0100

    Quick misaligned lw/sw test
    Looks like it works great

--- rom/rom.s
+++ rom/rom.s
@@ -4,14 +4,14 @@
 _start:
     li x1, 0x30000000
     li x2, 5
-    sb x2, 0(x1)
+    sw x2, 3(x1)
     li x3, 0x20000000
 
 loop:
         li x1, 0x30000000
-        lb x2, 0(x1)
+        lw x2, 3(x1)
         addi x2, x2, 1
-        sb x2, 0(x1)
+        sw x2, 3(x1)
         sb x2, 0(x3)
 
         li x4, 0x00400000
