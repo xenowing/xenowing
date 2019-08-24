22672bdd6f7ff6c932ae9aa0674e1dee3dc14d90
ferris <yupferris@gmail.com>
2019-08-24 16:17:03 +0200

    Hide uart regs

--- rom/xw/include/xw/uart.h
+++ rom/xw/include/xw/uart.h
@@ -3,9 +3,6 @@
 
 #include "inttypes.h"
 
-#define XW_UART_TX_STATUS ((volatile uint8_t *)0x21000000)
-#define XW_UART_TX_WRITE ((volatile uint8_t *)0x21000004)
-
 void xw_uart_write(uint8_t byte);
 
 void xw_putc(const char c);
--- rom/xw/src/uart.c
+++ rom/xw/src/uart.c
@@ -1,6 +1,9 @@
 #include <xw/bool.h>
 #include <xw/uart.h>
 
+#define XW_UART_TX_STATUS ((volatile uint8_t *)0x21000000)
+#define XW_UART_TX_WRITE ((volatile uint8_t *)0x21000004)
+
 void xw_uart_write(uint8_t byte)
 {
     while (!(*XW_UART_TX_STATUS & 1))
