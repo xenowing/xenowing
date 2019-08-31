09c832d1609dac7ce65c0a5958efd1866e4c2cca
ferris <yupferris@gmail.com>
2019-08-31 19:01:16 +0200

    Test double-buffering with CPU rendering
    Looks like it's fast, which is nice, but flickers like hell due to swapping buffers all the time with no vsync

--- rom/src/main.c
+++ rom/src/main.c
@@ -2,16 +2,14 @@
 
 #define FRAMEBUFFER_WIDTH 320
 #define FRAMEBUFFER_HEIGHT 240
-static uint16_t framebuffer[FRAMEBUFFER_WIDTH * FRAMEBUFFER_HEIGHT] __attribute__((aligned(8)));
+static uint16_t framebuffer0[FRAMEBUFFER_WIDTH * FRAMEBUFFER_HEIGHT] __attribute__((aligned(8)));
+static uint16_t framebuffer1[FRAMEBUFFER_WIDTH * FRAMEBUFFER_HEIGHT] __attribute__((aligned(8)));
 
 int main()
 {
     xw_puts("xw online");
 
     xw_display_init();
-
-    xw_puts("time to try a random framebuffer addr");
-    xw_display_set_framebuffer_addr(framebuffer);
 
     xw_puts("get ready for some 0xfadebabe");
     const uint32_t phase_bits = 8;
@@ -23,14 +21,16 @@ int main()
     bool duty_cycle_rising = true;
     xw_set_leds(0);
 
-    const uint32_t ticks_max = 2000;//500;
-    uint32_t ticks = 0;
-
-    uint32_t framebuffer_x = FRAMEBUFFER_WIDTH / 2;
-    uint32_t framebuffer_y = FRAMEBUFFER_HEIGHT / 2;
+    uint32_t framebuffer_x = 0;
+    uint32_t framebuffer_y = 0;
     bool framebuffer_x_rising = true;
     bool framebuffer_y_rising = true;
     uint16_t color = (31 << 11) | (63 << 5) | 31;
+    uint32_t box_size = 150;
+
+    uint16_t *back_buffer = framebuffer0;
+    uint16_t *front_buffer = framebuffer1;
+    xw_display_set_framebuffer_addr(front_buffer);
 
     while (true)
     {
@@ -41,56 +41,62 @@ int main()
         phase++;
         xw_set_leds((phase & phase_mask) < duty_cycle ? 1 : 0);
 
-        ticks++;
-        if (ticks >= ticks_max)
+        if (duty_cycle_rising)
         {
-            ticks = 0;
-            if (duty_cycle_rising)
-            {
-                duty_cycle++;
-                if (duty_cycle == phase_max)
-                    duty_cycle_rising = false;
-            }
-            else
-            {
-                duty_cycle--;
-                if (duty_cycle == 0)
-                    duty_cycle_rising = true;
-            }
-
-            for (int y = 0; y < FRAMEBUFFER_HEIGHT; y++)
-            {
-                for (int x = 0; x < FRAMEBUFFER_WIDTH; x++)
-                {
-                    framebuffer[y * FRAMEBUFFER_WIDTH + x] = 0;
-                }
-            }
-            /*if (framebuffer_x_rising)
-            {
-                framebuffer_x++;
-                if (framebuffer_x == FRAMEBUFFER_WIDTH - 1)
-                    framebuffer_x_rising = false;
-            }
-            else
-            {
-                framebuffer_x--;
-                if (framebuffer_x == 0)
-                    framebuffer_x_rising = true;
-            }
-            if (framebuffer_y_rising)
-            {
-                framebuffer_y++;
-                if (framebuffer_y == FRAMEBUFFER_HEIGHT - 1)
-                    framebuffer_y_rising = false;
-            }
-            else
-            {
-                framebuffer_y--;
-                if (framebuffer_y == 0)
-                    framebuffer_y_rising = true;
-            }*/
-            framebuffer[(framebuffer_y * FRAMEBUFFER_WIDTH) + framebuffer_x] = color;
+            duty_cycle++;
+            if (duty_cycle == phase_max)
+                duty_cycle_rising = false;
         }
+        else
+        {
+            duty_cycle--;
+            if (duty_cycle == 0)
+                duty_cycle_rising = true;
+        }
+
+        for (int y = 0; y < FRAMEBUFFER_HEIGHT; y++)
+        {
+            for (int x = 0; x < FRAMEBUFFER_WIDTH; x++)
+            {
+                back_buffer[y * FRAMEBUFFER_WIDTH + x] = 0;
+            }
+        }
+        if (framebuffer_x_rising)
+        {
+            framebuffer_x++;
+            if (framebuffer_x == FRAMEBUFFER_WIDTH - box_size)
+                framebuffer_x_rising = false;
+        }
+        else
+        {
+            framebuffer_x--;
+            if (framebuffer_x == 0)
+                framebuffer_x_rising = true;
+        }
+        if (framebuffer_y_rising)
+        {
+            framebuffer_y++;
+            if (framebuffer_y == FRAMEBUFFER_HEIGHT - box_size)
+                framebuffer_y_rising = false;
+        }
+        else
+        {
+            framebuffer_y--;
+            if (framebuffer_y == 0)
+                framebuffer_y_rising = true;
+        }
+        for (int y = 0; y < box_size; y++)
+        {
+            for (int x = 0; x < box_size; x++)
+            {
+                back_buffer[(framebuffer_y + y) * FRAMEBUFFER_WIDTH + framebuffer_x + x] = color;
+            }
+        }
+
+        uint16_t *temp = back_buffer;
+        back_buffer = front_buffer;
+        front_buffer = temp;
+        xw_display_set_framebuffer_addr(front_buffer);
     }
 
     return 0;
