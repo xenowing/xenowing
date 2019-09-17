df183e15aeee53c617841d34f5d97d43e46d221c
ferris <yupferris@gmail.com>
2019-09-17 21:09:48 +0200

    Improve reset synchronizer implementation
    This is more basic SV, but good practice for me. The first change is to accept a number of stages as a parameter, and clean up the code while I'm at it. The second major difference is to mark the sync chain regs with an inline attribute that helps the tool understand their purpose and place them close together so as to give the signal between the regs in the chain as much slack as possible, which allows them to better perform their jobs.

--- mimas_a7/test/uart/rtl/reset_synchronizer.sv
+++ mimas_a7/test/uart/rtl/reset_synchronizer.sv
@@ -6,16 +6,19 @@ module reset_synchronizer(
 
     output wire logic reset_n_sync);
 
-    logic sync_chain1, sync_chain2;
+    parameter STAGES = 2;
+
+    (* ASYNC_REG = "TRUE" *) logic [STAGES - 1:0] sync_chain;
+
     always_ff @(posedge clk, negedge reset_n) begin
         if (~reset_n) begin
-            { sync_chain1, sync_chain2 } <= 2'd0;
+            sync_chain[STAGES - 1:0] <= {STAGES{1'd0}};
         end
         else begin
-            { sync_chain2, sync_chain1 } <= { sync_chain1, 1'd1 };
+            sync_chain[STAGES - 1:0] <= {sync_chain[STAGES - 2:0], 1'd1};
         end
     end
 
-    assign reset_n_sync = sync_chain2;
+    assign reset_n_sync = sync_chain[STAGES - 1];
 
 endmodule
