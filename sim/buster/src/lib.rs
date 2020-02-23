d1a78acc00697fab25686550aee5fcae10993b8f
ferris <yupferris@gmail.com>
2020-02-23 22:14:56 +0100

    Duplicate buster master0 test for master1

--- sim/buster/src/lib.rs
+++ sim/buster/src/lib.rs
@@ -144,4 +144,65 @@ mod tests {
             m.posedge_clk();
         }
     }
+
+    #[test]
+    fn read_all_master1() {
+        let data = (0..65536).map(|x| (x + 3) * 12).collect::<Vec<_>>();
+
+        let mut master_read_addr = 0;
+        let mut master_read_data = Vec::new();
+
+        let mut slave_read_addr = None;
+
+        let mut m = Buster::new();
+
+        m.reset();
+
+        loop {
+            m.prop();
+
+            if m.master1_bus_read_data_valid {
+                master_read_data.push(m.master1_bus_read_data);
+                if master_read_data.len() == data.len() {
+                    assert_eq!(master_read_data, data);
+                    return;
+                }
+            }
+
+            assert_eq!(m.master0_bus_read_data_valid, false);
+
+            if let Some(addr) = slave_read_addr {
+                m.slave_bus_read_data = data[addr as usize];
+                m.slave_bus_read_data_valid = true;
+            } else {
+                m.slave_bus_read_data_valid = false;
+            }
+
+            if master_read_addr < data.len() {
+                m.master1_bus_enable = true;
+                m.master1_bus_addr = master_read_addr as _;
+            } else {
+                m.master1_bus_enable = false;
+            }
+            m.master0_bus_enable = false;
+            m.master0_bus_addr = 0;
+
+            m.slave_bus_ready = true;
+
+            m.prop();
+
+            if m.master1_bus_enable && m.master1_bus_ready {
+                master_read_addr += 1;
+            }
+
+            slave_read_addr = if m.slave_bus_enable {
+                Some(m.slave_bus_addr)
+            } else {
+                None
+            };
+
+            m.prop();
+            m.posedge_clk();
+        }
+    }
 }
