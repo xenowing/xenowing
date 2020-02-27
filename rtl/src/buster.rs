ed1c59b7bebcedbd25c6657d20e20e71830f2e66
ferris <yupferris@gmail.com>
2020-02-27 19:43:30 +0100

    Change master/slave -> primary/replica
    Also add diagram for preliminary Buster design

--- rtl/src/buster.rs
+++ rtl/src/buster.rs
@@ -8,112 +8,112 @@ pub fn generate<'a, S: Into<String>>(c: &'a Context<'a>, mod_name: S, addr_bit_w
     {
         let m = c.module(format!("{}IssueArbiter", mod_name));
 
-        let master0_bus_enable = m.input("master0_bus_enable", 1);
-        let master0_bus_addr = m.input("master0_bus_addr", addr_bit_width);
+        let primary0_bus_enable = m.input("primary0_bus_enable", 1);
+        let primary0_bus_addr = m.input("primary0_bus_addr", addr_bit_width);
 
-        let master1_bus_enable = m.input("master1_bus_enable", 1);
-        let master1_bus_addr = m.input("master1_bus_addr", addr_bit_width);
+        let primary1_bus_enable = m.input("primary1_bus_enable", 1);
+        let primary1_bus_addr = m.input("primary1_bus_addr", addr_bit_width);
 
         let issue_bus_ready = m.input("issue_bus_ready", 1);
 
-        let (bus_enable, bus_master, bus_addr, master1_bus_ready) = if_(master0_bus_enable, {
-            (m.high(), m.low(), master0_bus_addr, m.low())
+        let (bus_enable, bus_primary, bus_addr, primary1_bus_ready) = if_(primary0_bus_enable, {
+            (m.high(), m.low(), primary0_bus_addr, m.low())
         }).else_({
-            (master1_bus_enable, m.high(), master1_bus_addr, issue_bus_ready)
+            (primary1_bus_enable, m.high(), primary1_bus_addr, issue_bus_ready)
         });
 
         m.output("issue_bus_enable", bus_enable);
-        m.output("issue_bus_master", bus_master);
+        m.output("issue_bus_primary", bus_primary);
         m.output("issue_bus_addr", bus_addr);
 
-        m.output("master0_bus_ready", issue_bus_ready);
-        m.output("master1_bus_ready", master1_bus_ready);
+        m.output("primary0_bus_ready", issue_bus_ready);
+        m.output("primary1_bus_ready", primary1_bus_ready);
     }
 
     {
         let m = c.module(format!("{}Issue", mod_name));
 
         let issue_arb_bus_enable = m.input("issue_arb_bus_enable", 1);
-        let issue_arb_bus_master = m.input("issue_arb_bus_master", 1);
+        let issue_arb_bus_primary = m.input("issue_arb_bus_primary", 1);
         let issue_arb_bus_addr = m.input("issue_arb_bus_addr", addr_bit_width);
 
-        let slave_bus_ready = m.input("slave_bus_ready", 1);
-        let master_fifo_full = m.input("master_fifo_full", 1);
-        let master_fifo_write_ready = !master_fifo_full;
+        let replica_bus_ready = m.input("replica_bus_ready", 1);
+        let primary_fifo_full = m.input("primary_fifo_full", 1);
+        let primary_fifo_write_ready = !primary_fifo_full;
 
-        m.output("issue_arb_bus_ready", master_fifo_write_ready & slave_bus_ready);
+        m.output("issue_arb_bus_ready", primary_fifo_write_ready & replica_bus_ready);
 
-        m.output("slave_bus_enable", issue_arb_bus_enable & master_fifo_write_ready);
-        m.output("slave_bus_addr", issue_arb_bus_addr);
+        m.output("replica_bus_enable", issue_arb_bus_enable & primary_fifo_write_ready);
+        m.output("replica_bus_addr", issue_arb_bus_addr);
 
-        m.output("master_fifo_write_enable", issue_arb_bus_enable & master_fifo_write_ready & slave_bus_ready);
-        m.output("master_fifo_write_data", issue_arb_bus_master);
+        m.output("primary_fifo_write_enable", issue_arb_bus_enable & primary_fifo_write_ready & replica_bus_ready);
+        m.output("primary_fifo_write_data", issue_arb_bus_primary);
     }
 
     {
         let m = c.module(format!("{}ReturnArbiter", mod_name));
 
-        let master_fifo_empty = m.input("master_fifo_empty", 1);
-        let master_fifo_read_ready = !master_fifo_empty;
-        let master_fifo_read_data = m.input("master_fifo_read_data", 1);
+        let primary_fifo_empty = m.input("primary_fifo_empty", 1);
+        let primary_fifo_read_ready = !primary_fifo_empty;
+        let primary_fifo_read_data = m.input("primary_fifo_read_data", 1);
         let data_fifo_empty = m.input("data_fifo_empty", 1);
         let data_fifo_read_ready = !data_fifo_empty;
         let data_fifo_read_data = m.input("data_fifo_read_data", data_bit_width);
 
-        let fifo_read_enable = master_fifo_read_ready & data_fifo_read_ready;
+        let fifo_read_enable = primary_fifo_read_ready & data_fifo_read_ready;
         m.output("fifo_read_enable", fifo_read_enable);
 
         let fifo_read_data_valid = m.reg("fifo_read_data_valid", 1);
         fifo_read_data_valid.default_value(false);
         fifo_read_data_valid.drive_next(fifo_read_enable);
 
-        m.output("master0_bus_read_data", data_fifo_read_data);
-        m.output("master0_bus_read_data_valid", fifo_read_data_valid.value & !master_fifo_read_data);
-        m.output("master1_bus_read_data", data_fifo_read_data);
-        m.output("master1_bus_read_data_valid", fifo_read_data_valid.value & master_fifo_read_data);
+        m.output("primary0_bus_read_data", data_fifo_read_data);
+        m.output("primary0_bus_read_data_valid", fifo_read_data_valid.value & !primary_fifo_read_data);
+        m.output("primary1_bus_read_data", data_fifo_read_data);
+        m.output("primary1_bus_read_data_valid", fifo_read_data_valid.value & primary_fifo_read_data);
     }
 
     let m = c.module(mod_name.clone());
 
     let issue_arbiter = m.instance("issue_arbiter", &format!("{}IssueArbiter", mod_name));
-    m.output("master0_bus_ready", issue_arbiter.output("master0_bus_ready"));
-    issue_arbiter.drive_input("master0_bus_enable", m.input("master0_bus_enable", 1));
-    issue_arbiter.drive_input("master0_bus_addr", m.input("master0_bus_addr", addr_bit_width));
-    m.output("master1_bus_ready", issue_arbiter.output("master1_bus_ready"));
-    issue_arbiter.drive_input("master1_bus_enable", m.input("master1_bus_enable", 1));
-    issue_arbiter.drive_input("master1_bus_addr", m.input("master1_bus_addr", addr_bit_width));
+    m.output("primary0_bus_ready", issue_arbiter.output("primary0_bus_ready"));
+    issue_arbiter.drive_input("primary0_bus_enable", m.input("primary0_bus_enable", 1));
+    issue_arbiter.drive_input("primary0_bus_addr", m.input("primary0_bus_addr", addr_bit_width));
+    m.output("primary1_bus_ready", issue_arbiter.output("primary1_bus_ready"));
+    issue_arbiter.drive_input("primary1_bus_enable", m.input("primary1_bus_enable", 1));
+    issue_arbiter.drive_input("primary1_bus_addr", m.input("primary1_bus_addr", addr_bit_width));
 
     let issue = m.instance("issue", &format!("{}Issue", mod_name));
     issue.drive_input("issue_arb_bus_enable", issue_arbiter.output("issue_bus_enable"));
-    issue.drive_input("issue_arb_bus_master", issue_arbiter.output("issue_bus_master"));
+    issue.drive_input("issue_arb_bus_primary", issue_arbiter.output("issue_bus_primary"));
     issue.drive_input("issue_arb_bus_addr", issue_arbiter.output("issue_bus_addr"));
-    issue.drive_input("slave_bus_ready", m.input("slave_bus_ready", 1));
+    issue.drive_input("replica_bus_ready", m.input("replica_bus_ready", 1));
     issue_arbiter.drive_input("issue_bus_ready", issue.output("issue_arb_bus_ready"));
-    m.output("slave_bus_enable", issue.output("slave_bus_enable"));
-    m.output("slave_bus_addr", issue.output("slave_bus_addr"));
+    m.output("replica_bus_enable", issue.output("replica_bus_enable"));
+    m.output("replica_bus_addr", issue.output("replica_bus_addr"));
 
-    fifo::generate(&c, &format!("{}MasterFifo", mod_name), fifo_depth_bits, 1);
-    let master_fifo = m.instance("master_fifo", &format!("{}MasterFifo", mod_name));
-    issue.drive_input("master_fifo_full", master_fifo.output("full"));
-    master_fifo.drive_input("write_enable", issue.output("master_fifo_write_enable"));
-    master_fifo.drive_input("write_data", issue.output("master_fifo_write_data"));
+    fifo::generate(&c, &format!("{}PrimaryFifo", mod_name), fifo_depth_bits, 1);
+    let primary_fifo = m.instance("primary_fifo", &format!("{}PrimaryFifo", mod_name));
+    issue.drive_input("primary_fifo_full", primary_fifo.output("full"));
+    primary_fifo.drive_input("write_enable", issue.output("primary_fifo_write_enable"));
+    primary_fifo.drive_input("write_data", issue.output("primary_fifo_write_data"));
 
     fifo::generate(&c, &format!("{}DataFifo", mod_name), fifo_depth_bits, data_bit_width);
     let data_fifo = m.instance("data_fifo", &format!("{}DataFifo", mod_name));
-    data_fifo.drive_input("write_enable", m.input("slave_bus_read_data_valid", 1));
-    data_fifo.drive_input("write_data", m.input("slave_bus_read_data", data_bit_width));
+    data_fifo.drive_input("write_enable", m.input("replica_bus_read_data_valid", 1));
+    data_fifo.drive_input("write_data", m.input("replica_bus_read_data", data_bit_width));
 
     let return_arbiter = m.instance("return_arbiter", "BusterReturnArbiter");
-    return_arbiter.drive_input("master_fifo_empty", master_fifo.output("empty"));
-    master_fifo.drive_input("read_enable", return_arbiter.output("fifo_read_enable"));
-    return_arbiter.drive_input("master_fifo_read_data", master_fifo.output("read_data"));
+    return_arbiter.drive_input("primary_fifo_empty", primary_fifo.output("empty"));
+    primary_fifo.drive_input("read_enable", return_arbiter.output("fifo_read_enable"));
+    return_arbiter.drive_input("primary_fifo_read_data", primary_fifo.output("read_data"));
     return_arbiter.drive_input("data_fifo_empty", data_fifo.output("empty"));
     data_fifo.drive_input("read_enable", return_arbiter.output("fifo_read_enable"));
     return_arbiter.drive_input("data_fifo_read_data", data_fifo.output("read_data"));
-    m.output("master0_bus_read_data", return_arbiter.output("master0_bus_read_data"));
-    m.output("master0_bus_read_data_valid", return_arbiter.output("master0_bus_read_data_valid"));
-    m.output("master1_bus_read_data", return_arbiter.output("master1_bus_read_data"));
-    m.output("master1_bus_read_data_valid", return_arbiter.output("master1_bus_read_data_valid"));
+    m.output("primary0_bus_read_data", return_arbiter.output("primary0_bus_read_data"));
+    m.output("primary0_bus_read_data_valid", return_arbiter.output("primary0_bus_read_data_valid"));
+    m.output("primary1_bus_read_data", return_arbiter.output("primary1_bus_read_data"));
+    m.output("primary1_bus_read_data_valid", return_arbiter.output("primary1_bus_read_data_valid"));
 
     m
 }
--- sim/buster/src/lib.rs
+++ sim/buster/src/lib.rs
@@ -7,91 +7,91 @@ mod tests {
     use modules::*;
 
     #[test]
-    fn single_read_master0() {
+    fn single_read_primary0() {
         let mut m = Buster::new();
 
         m.reset();
 
-        m.master0_bus_enable = true;
-        m.master0_bus_addr = 0xbabe;
-        m.master1_bus_enable = false;
-        m.master1_bus_addr = 0;
-        m.slave_bus_ready = true;
-        m.slave_bus_read_data = 0xffffffffffffffffffffffffffffffff;
-        m.slave_bus_read_data_valid = false;
+        m.primary0_bus_enable = true;
+        m.primary0_bus_addr = 0xbabe;
+        m.primary1_bus_enable = false;
+        m.primary1_bus_addr = 0;
+        m.replica_bus_ready = true;
+        m.replica_bus_read_data = 0xffffffffffffffffffffffffffffffff;
+        m.replica_bus_read_data_valid = false;
 
         m.prop();
 
-        assert_eq!(m.master0_bus_ready, true);
-        assert_eq!(m.master1_bus_ready, false);
-        assert_eq!(m.slave_bus_enable, true);
-        assert_eq!(m.slave_bus_addr, 0xbabe);
+        assert_eq!(m.primary0_bus_ready, true);
+        assert_eq!(m.primary1_bus_ready, false);
+        assert_eq!(m.replica_bus_enable, true);
+        assert_eq!(m.replica_bus_addr, 0xbabe);
 
         m.posedge_clk();
 
-        m.master0_bus_enable = false;
-        m.slave_bus_read_data = 0xdeadbeeffadebabeabad1deacafebabe;
-        m.slave_bus_read_data_valid = true;
+        m.primary0_bus_enable = false;
+        m.replica_bus_read_data = 0xdeadbeeffadebabeabad1deacafebabe;
+        m.replica_bus_read_data_valid = true;
 
         m.prop();
 
-        while !m.master0_bus_read_data_valid {
+        while !m.primary0_bus_read_data_valid {
             m.posedge_clk();
             m.prop();
         }
 
-        assert_eq!(m.master0_bus_read_data, 0xdeadbeeffadebabeabad1deacafebabe);
+        assert_eq!(m.primary0_bus_read_data, 0xdeadbeeffadebabeabad1deacafebabe);
 
-        assert_eq!(m.master1_bus_read_data_valid, false);
+        assert_eq!(m.primary1_bus_read_data_valid, false);
     }
 
     #[test]
-    fn single_read_master1() {
+    fn single_read_primary1() {
         let mut m = Buster::new();
 
         m.reset();
 
-        m.master0_bus_enable = false;
-        m.master0_bus_addr = 0;
-        m.master1_bus_enable = true;
-        m.master1_bus_addr = 0xbeef;
-        m.slave_bus_ready = true;
-        m.slave_bus_read_data = 0xffffffffffffffffffffffffffffffff;
-        m.slave_bus_read_data_valid = false;
+        m.primary0_bus_enable = false;
+        m.primary0_bus_addr = 0;
+        m.primary1_bus_enable = true;
+        m.primary1_bus_addr = 0xbeef;
+        m.replica_bus_ready = true;
+        m.replica_bus_read_data = 0xffffffffffffffffffffffffffffffff;
+        m.replica_bus_read_data_valid = false;
 
         m.prop();
 
-        assert_eq!(m.master0_bus_ready, true);
-        assert_eq!(m.master1_bus_ready, true);
-        assert_eq!(m.slave_bus_enable, true);
-        assert_eq!(m.slave_bus_addr, 0xbeef);
+        assert_eq!(m.primary0_bus_ready, true);
+        assert_eq!(m.primary1_bus_ready, true);
+        assert_eq!(m.replica_bus_enable, true);
+        assert_eq!(m.replica_bus_addr, 0xbeef);
 
         m.posedge_clk();
 
-        m.master1_bus_enable = false;
-        m.slave_bus_read_data = 0xdeadbeeffadebabeabad1deacafebabe;
-        m.slave_bus_read_data_valid = true;
+        m.primary1_bus_enable = false;
+        m.replica_bus_read_data = 0xdeadbeeffadebabeabad1deacafebabe;
+        m.replica_bus_read_data_valid = true;
 
         m.prop();
 
-        while !m.master1_bus_read_data_valid {
+        while !m.primary1_bus_read_data_valid {
             m.posedge_clk();
             m.prop();
         }
 
-        assert_eq!(m.master1_bus_read_data, 0xdeadbeeffadebabeabad1deacafebabe);
+        assert_eq!(m.primary1_bus_read_data, 0xdeadbeeffadebabeabad1deacafebabe);
 
-        assert_eq!(m.master0_bus_read_data_valid, false);
+        assert_eq!(m.primary0_bus_read_data_valid, false);
     }
 
     #[test]
-    fn read_all_master0() {
+    fn read_all_primary0() {
         let data = (0..65536).map(|x| (x + 1) * 4).collect::<Vec<_>>();
 
-        let mut master_read_addr = 0;
-        let mut master_read_data = Vec::new();
+        let mut primary_read_addr = 0;
+        let mut primary_read_data = Vec::new();
 
-        let mut slave_read_addr = None;
+        let mut replica_read_addr = None;
 
         let mut m = Buster::new();
 
@@ -100,42 +100,42 @@ mod tests {
         loop {
             m.prop();
 
-            if m.master0_bus_read_data_valid {
-                master_read_data.push(m.master0_bus_read_data);
-                if master_read_data.len() == data.len() {
-                    assert_eq!(master_read_data, data);
+            if m.primary0_bus_read_data_valid {
+                primary_read_data.push(m.primary0_bus_read_data);
+                if primary_read_data.len() == data.len() {
+                    assert_eq!(primary_read_data, data);
                     return;
                 }
             }
 
-            assert_eq!(m.master1_bus_read_data_valid, false);
+            assert_eq!(m.primary1_bus_read_data_valid, false);
 
-            if let Some(addr) = slave_read_addr {
-                m.slave_bus_read_data = data[addr as usize];
-                m.slave_bus_read_data_valid = true;
+            if let Some(addr) = replica_read_addr {
+                m.replica_bus_read_data = data[addr as usize];
+                m.replica_bus_read_data_valid = true;
             } else {
-                m.slave_bus_read_data_valid = false;
+                m.replica_bus_read_data_valid = false;
             }
 
-            if master_read_addr < data.len() {
-                m.master0_bus_enable = true;
-                m.master0_bus_addr = master_read_addr as _;
+            if primary_read_addr < data.len() {
+                m.primary0_bus_enable = true;
+                m.primary0_bus_addr = primary_read_addr as _;
             } else {
-                m.master0_bus_enable = false;
+                m.primary0_bus_enable = false;
             }
-            m.master1_bus_enable = false;
-            m.master1_bus_addr = 0;
+            m.primary1_bus_enable = false;
+            m.primary1_bus_addr = 0;
 
-            m.slave_bus_ready = true;
+            m.replica_bus_ready = true;
 
             m.prop();
 
-            if m.master0_bus_enable && m.master0_bus_ready {
-                master_read_addr += 1;
+            if m.primary0_bus_enable && m.primary0_bus_ready {
+                primary_read_addr += 1;
             }
 
-            slave_read_addr = if m.slave_bus_enable {
-                Some(m.slave_bus_addr)
+            replica_read_addr = if m.replica_bus_enable {
+                Some(m.replica_bus_addr)
             } else {
                 None
             };
@@ -146,13 +146,13 @@ mod tests {
     }
 
     #[test]
-    fn read_all_master1() {
+    fn read_all_primary1() {
         let data = (0..65536).map(|x| (x + 3) * 12).collect::<Vec<_>>();
 
-        let mut master_read_addr = 0;
-        let mut master_read_data = Vec::new();
+        let mut primary_read_addr = 0;
+        let mut primary_read_data = Vec::new();
 
-        let mut slave_read_addr = None;
+        let mut replica_read_addr = None;
 
         let mut m = Buster::new();
 
@@ -161,42 +161,42 @@ mod tests {
         loop {
             m.prop();
 
-            if m.master1_bus_read_data_valid {
-                master_read_data.push(m.master1_bus_read_data);
-                if master_read_data.len() == data.len() {
-                    assert_eq!(master_read_data, data);
+            if m.primary1_bus_read_data_valid {
+                primary_read_data.push(m.primary1_bus_read_data);
+                if primary_read_data.len() == data.len() {
+                    assert_eq!(primary_read_data, data);
                     return;
                 }
             }
 
-            assert_eq!(m.master0_bus_read_data_valid, false);
+            assert_eq!(m.primary0_bus_read_data_valid, false);
 
-            if let Some(addr) = slave_read_addr {
-                m.slave_bus_read_data = data[addr as usize];
-                m.slave_bus_read_data_valid = true;
+            if let Some(addr) = replica_read_addr {
+                m.replica_bus_read_data = data[addr as usize];
+                m.replica_bus_read_data_valid = true;
             } else {
-                m.slave_bus_read_data_valid = false;
+                m.replica_bus_read_data_valid = false;
             }
 
-            if master_read_addr < data.len() {
-                m.master1_bus_enable = true;
-                m.master1_bus_addr = master_read_addr as _;
+            if primary_read_addr < data.len() {
+                m.primary1_bus_enable = true;
+                m.primary1_bus_addr = primary_read_addr as _;
             } else {
-                m.master1_bus_enable = false;
+                m.primary1_bus_enable = false;
             }
-            m.master0_bus_enable = false;
-            m.master0_bus_addr = 0;
+            m.primary0_bus_enable = false;
+            m.primary0_bus_addr = 0;
 
-            m.slave_bus_ready = true;
+            m.replica_bus_ready = true;
 
             m.prop();
 
-            if m.master1_bus_enable && m.master1_bus_ready {
-                master_read_addr += 1;
+            if m.primary1_bus_enable && m.primary1_bus_ready {
+                primary_read_addr += 1;
             }
 
-            slave_read_addr = if m.slave_bus_enable {
-                Some(m.slave_bus_addr)
+            replica_read_addr = if m.replica_bus_enable {
+                Some(m.replica_bus_addr)
             } else {
                 None
             };
