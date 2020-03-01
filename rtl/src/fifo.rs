fe1b6a40dac446189a8f40b6fb0d14dc736ac888
ferris <yupferris@gmail.com>
2020-03-01 14:59:04 +0100

    Small FIFO code cleanups
    Mainly using 'bit_width' instead of 'num_bits' for consistency and not specifying the returned reference lifetime.

--- rtl/src/fifo.rs
+++ rtl/src/fifo.rs
@@ -1,11 +1,11 @@
 use kaze::*;
 
-pub fn generate<'a, S: Into<String>>(c: &'a Context<'a>, mod_name: S, num_depth_bits: u32, num_element_bits: u32) -> &'a Module<'a> {
+pub fn generate<'a, S: Into<String>>(c: &'a Context<'a>, mod_name: S, depth_bit_width: u32, element_bit_width: u32) -> &Module<'a> {
     let m = c.module(mod_name);
 
-    let mem = m.mem("mem", num_depth_bits, num_element_bits);
+    let mem = m.mem("mem", depth_bit_width, element_bit_width);
 
-    let count_bits = num_depth_bits + 1;
+    let count_bits = depth_bit_width + 1;
     let count = m.reg("count", count_bits);
     count.default_value(0u32);
 
@@ -17,13 +17,13 @@ pub fn generate<'a, S: Into<String>>(c: &'a Context<'a>, mod_name: S, num_depth_
 
     let write_enable = m.input("write_enable", 1) & !full;
 
-    let mem_write_addr = m.reg("mem_write_addr", num_depth_bits);
+    let mem_write_addr = m.reg("mem_write_addr", depth_bit_width);
     mem_write_addr.default_value(0u32);
 
-    mem.write_port(mem_write_addr.value, m.input("write_data", num_element_bits), write_enable);
+    mem.write_port(mem_write_addr.value, m.input("write_data", element_bit_width), write_enable);
 
     let (next_count, next_mem_write_addr) = if_(write_enable, {
-        (next_count + m.lit(1u32, count_bits), mem_write_addr.value + m.lit(1u32, num_depth_bits))
+        (next_count + m.lit(1u32, count_bits), mem_write_addr.value + m.lit(1u32, depth_bit_width))
     }).else_({
         (next_count, mem_write_addr.value)
     });
@@ -40,13 +40,13 @@ pub fn generate<'a, S: Into<String>>(c: &'a Context<'a>, mod_name: S, num_depth_
 
     let read_enable = m.input("read_enable", 1) & !empty;
 
-    let mem_read_addr = m.reg("mem_read_addr", num_depth_bits);
+    let mem_read_addr = m.reg("mem_read_addr", depth_bit_width);
     mem_read_addr.default_value(0u32);
 
     m.output("read_data", mem.read_port(mem_read_addr.value, read_enable));
 
     let (next_count, next_mem_read_addr) = if_(read_enable, {
-        (next_count - m.lit(1u32, count_bits), mem_read_addr.value + m.lit(1u32, num_depth_bits))
+        (next_count - m.lit(1u32, count_bits), mem_read_addr.value + m.lit(1u32, depth_bit_width))
     }).else_({
         (next_count, mem_read_addr.value)
     });
