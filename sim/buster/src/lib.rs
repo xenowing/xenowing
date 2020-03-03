73d705b4c910e5f3ec94a5056edf4b4a5b1290da
ferris <yupferris@gmail.com>
2020-03-03 22:08:51 +0100

    1x2 buster tests work
    Literally copy/paste'd the 2x2 tests and removed the primary1 stuff, and it just worked, as expected.

--- sim/buster/src/lib.rs
+++ sim/buster/src/lib.rs
@@ -7,7 +7,194 @@ mod tests {
     use modules::*;
 
     #[test]
-    fn single_read_primary0_replica0() {
+    fn buster1x2_single_read_replica0() {
+        let mut m = Buster1x2::new();
+
+        m.reset();
+
+        m.primary0_bus_enable = true;
+        m.primary0_bus_addr = 0x0babe;
+        m.replica0_bus_ready = true;
+        m.replica1_bus_ready = false;
+        m.replica_bus_read_data = 0xffffffff;
+        m.replica_bus_read_data_valid = false;
+
+        m.prop();
+
+        assert_eq!(m.primary0_bus_ready, true);
+        assert_eq!(m.replica0_bus_enable, true);
+        assert_eq!(m.replica1_bus_enable, false);
+        assert_eq!(m.replica0_bus_addr, 0xbabe);
+
+        m.posedge_clk();
+
+        m.primary0_bus_enable = false;
+        m.replica_bus_read_data = 0xfadebabe;
+        m.replica_bus_read_data_valid = true;
+
+        m.prop();
+
+        while !m.primary0_bus_read_data_valid {
+            m.posedge_clk();
+            m.prop();
+        }
+
+        assert_eq!(m.primary0_bus_read_data, 0xfadebabe);
+    }
+
+    #[test]
+    fn buster1x2_single_read_replica1() {
+        let mut m = Buster1x2::new();
+
+        m.reset();
+
+        m.primary0_bus_enable = true;
+        m.primary0_bus_addr = 0x1babe;
+        m.replica0_bus_ready = false;
+        m.replica1_bus_ready = true;
+        m.replica_bus_read_data = 0xffffffff;
+        m.replica_bus_read_data_valid = false;
+
+        m.prop();
+
+        assert_eq!(m.primary0_bus_ready, true);
+        assert_eq!(m.replica0_bus_enable, false);
+        assert_eq!(m.replica1_bus_enable, true);
+        assert_eq!(m.replica1_bus_addr, 0xbabe);
+
+        m.posedge_clk();
+
+        m.primary0_bus_enable = false;
+        m.replica_bus_read_data = 0xfadebabe;
+        m.replica_bus_read_data_valid = true;
+
+        m.prop();
+
+        while !m.primary0_bus_read_data_valid {
+            m.posedge_clk();
+            m.prop();
+        }
+
+        assert_eq!(m.primary0_bus_read_data, 0xfadebabe);
+    }
+
+    #[test]
+    fn buster1x2_read_all_replica0() {
+        let data = (0..65536).map(|x| (x + 1) * 4).collect::<Vec<_>>();
+
+        let mut primary_read_addr = 0;
+        let mut primary_read_data = Vec::new();
+
+        let mut replica_read_addr = None;
+
+        let mut m = Buster1x2::new();
+
+        m.reset();
+
+        loop {
+            m.prop();
+
+            if m.primary0_bus_read_data_valid {
+                primary_read_data.push(m.primary0_bus_read_data);
+                if primary_read_data.len() == data.len() {
+                    assert_eq!(primary_read_data, data);
+                    return;
+                }
+            }
+
+
+            if let Some(addr) = replica_read_addr {
+                m.replica_bus_read_data = data[addr as usize];
+                m.replica_bus_read_data_valid = true;
+            } else {
+                m.replica_bus_read_data_valid = false;
+            }
+
+            if primary_read_addr < data.len() {
+                m.primary0_bus_enable = true;
+                m.primary0_bus_addr = primary_read_addr as _;
+            } else {
+                m.primary0_bus_enable = false;
+            }
+
+            m.replica0_bus_ready = true;
+
+            m.prop();
+
+            if m.primary0_bus_enable && m.primary0_bus_ready {
+                primary_read_addr += 1;
+            }
+
+            replica_read_addr = if m.replica0_bus_enable {
+                Some(m.replica0_bus_addr)
+            } else {
+                None
+            };
+
+            m.prop();
+            m.posedge_clk();
+        }
+    }
+
+    #[test]
+    fn buster1x2_read_all_replica1() {
+        let data = (0..65536).map(|x| (x + 1) * 4).collect::<Vec<_>>();
+
+        let mut primary_read_addr = 0;
+        let mut primary_read_data = Vec::new();
+
+        let mut replica_read_addr = None;
+
+        let mut m = Buster1x2::new();
+
+        m.reset();
+
+        loop {
+            m.prop();
+
+            if m.primary0_bus_read_data_valid {
+                primary_read_data.push(m.primary0_bus_read_data);
+                if primary_read_data.len() == data.len() {
+                    assert_eq!(primary_read_data, data);
+                    return;
+                }
+            }
+
+            if let Some(addr) = replica_read_addr {
+                m.replica_bus_read_data = data[addr as usize];
+                m.replica_bus_read_data_valid = true;
+            } else {
+                m.replica_bus_read_data_valid = false;
+            }
+
+            if primary_read_addr < data.len() {
+                m.primary0_bus_enable = true;
+                m.primary0_bus_addr = 0x10000 | primary_read_addr as u32;
+            } else {
+                m.primary0_bus_enable = false;
+            }
+
+            m.replica1_bus_ready = true;
+
+            m.prop();
+
+            if m.primary0_bus_enable && m.primary0_bus_ready {
+                primary_read_addr += 1;
+            }
+
+            replica_read_addr = if m.replica1_bus_enable {
+                Some(m.replica1_bus_addr)
+            } else {
+                None
+            };
+
+            m.prop();
+            m.posedge_clk();
+        }
+    }
+
+    #[test]
+    fn buster2x2_single_read_primary0_replica0() {
         let mut m = Buster2x2::new();
 
         m.reset();
@@ -48,48 +235,7 @@ mod tests {
     }
 
     #[test]
-    fn single_read_primary1_replica0() {
-        let mut m = Buster2x2::new();
-
-        m.reset();
-
-        m.primary0_bus_enable = false;
-        m.primary0_bus_addr = 0;
-        m.primary1_bus_enable = true;
-        m.primary1_bus_addr = 0x0beef;
-        m.replica0_bus_ready = true;
-        m.replica1_bus_ready = false;
-        m.replica_bus_read_data = 0xffffffffffffffffffffffffffffffff;
-        m.replica_bus_read_data_valid = false;
-
-        m.prop();
-
-        assert_eq!(m.primary0_bus_ready, true);
-        assert_eq!(m.primary1_bus_ready, true);
-        assert_eq!(m.replica0_bus_enable, true);
-        assert_eq!(m.replica1_bus_enable, false);
-        assert_eq!(m.replica0_bus_addr, 0xbeef);
-
-        m.posedge_clk();
-
-        m.primary1_bus_enable = false;
-        m.replica_bus_read_data = 0xdeadbeeffadebabeabad1deacafebabe;
-        m.replica_bus_read_data_valid = true;
-
-        m.prop();
-
-        while !m.primary1_bus_read_data_valid {
-            m.posedge_clk();
-            m.prop();
-        }
-
-        assert_eq!(m.primary1_bus_read_data, 0xdeadbeeffadebabeabad1deacafebabe);
-
-        assert_eq!(m.primary0_bus_read_data_valid, false);
-    }
-
-    #[test]
-    fn single_read_primary0_replica1() {
+    fn buster2x2_single_read_primary0_replica1() {
         let mut m = Buster2x2::new();
 
         m.reset();
@@ -130,7 +276,48 @@ mod tests {
     }
 
     #[test]
-    fn single_read_primary1_replica1() {
+    fn buster2x2_single_read_primary1_replica0() {
+        let mut m = Buster2x2::new();
+
+        m.reset();
+
+        m.primary0_bus_enable = false;
+        m.primary0_bus_addr = 0;
+        m.primary1_bus_enable = true;
+        m.primary1_bus_addr = 0x0beef;
+        m.replica0_bus_ready = true;
+        m.replica1_bus_ready = false;
+        m.replica_bus_read_data = 0xffffffffffffffffffffffffffffffff;
+        m.replica_bus_read_data_valid = false;
+
+        m.prop();
+
+        assert_eq!(m.primary0_bus_ready, true);
+        assert_eq!(m.primary1_bus_ready, true);
+        assert_eq!(m.replica0_bus_enable, true);
+        assert_eq!(m.replica1_bus_enable, false);
+        assert_eq!(m.replica0_bus_addr, 0xbeef);
+
+        m.posedge_clk();
+
+        m.primary1_bus_enable = false;
+        m.replica_bus_read_data = 0xdeadbeeffadebabeabad1deacafebabe;
+        m.replica_bus_read_data_valid = true;
+
+        m.prop();
+
+        while !m.primary1_bus_read_data_valid {
+            m.posedge_clk();
+            m.prop();
+        }
+
+        assert_eq!(m.primary1_bus_read_data, 0xdeadbeeffadebabeabad1deacafebabe);
+
+        assert_eq!(m.primary0_bus_read_data_valid, false);
+    }
+
+    #[test]
+    fn buster2x2_single_read_primary1_replica1() {
         let mut m = Buster2x2::new();
 
         m.reset();
@@ -171,7 +358,7 @@ mod tests {
     }
 
     #[test]
-    fn read_all_primary0_replica0() {
+    fn buster2x2_read_all_primary0_replica0() {
         let data = (0..65536).map(|x| (x + 1) * 4).collect::<Vec<_>>();
 
         let mut primary_read_addr = 0;
@@ -232,7 +419,7 @@ mod tests {
     }
 
     #[test]
-    fn read_all_primary0_replica1() {
+    fn buster2x2_read_all_primary0_replica1() {
         let data = (0..65536).map(|x| (x + 1) * 4).collect::<Vec<_>>();
 
         let mut primary_read_addr = 0;
@@ -293,7 +480,7 @@ mod tests {
     }
 
     #[test]
-    fn read_all_primary1_replica0() {
+    fn buster2x2_read_all_primary1_replica0() {
         let data = (0..65536).map(|x| (x + 3) * 12).collect::<Vec<_>>();
 
         let mut primary_read_addr = 0;
@@ -354,7 +541,7 @@ mod tests {
     }
 
     #[test]
-    fn read_all_primary1_replica1() {
+    fn buster2x2_read_all_primary1_replica1() {
         let data = (0..65536).map(|x| (x + 3) * 12).collect::<Vec<_>>();
 
         let mut primary_read_addr = 0;
