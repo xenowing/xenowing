4f87f0914a7d21403a8d91de362d7e1e587acec1
ferris <yupferris@gmail.com>
2020-03-14 15:52:18 +0100

    Merge reset and rx sync code in uart test
    I've also moved the SyncChain rtl file out into a board-specific rtl dir instead of a test-specific one, and I plan to reuse this code for the ddr test as well when I pick that up again.

--- mimas_a7/rtl/SyncChain.sv
+++ mimas_a7/rtl/SyncChain.sv
@@ -1,24 +1,24 @@
 `default_nettype none
 
-module reset_synchronizer(
+module SyncChain#(parameter STAGES = 2, DEFAULT = 1'b0) (
     input wire logic reset_n,
     input wire logic clk,
 
-    output wire logic reset_n_sync);
+    input wire logic x,
 
-    parameter STAGES = 2;
+    output wire logic x_sync);
 
     (* ASYNC_REG = "TRUE" *) logic [STAGES - 1:0] sync_chain;
 
     always_ff @(posedge clk, negedge reset_n) begin
         if (~reset_n) begin
-            sync_chain <= {STAGES{1'd0}};
+            sync_chain <= {STAGES{DEFAULT}};
         end
         else begin
-            sync_chain <= {sync_chain[STAGES - 2:0], 1'd1};
+            sync_chain <= {sync_chain[STAGES - 2:0], x};
         end
     end
 
-    assign reset_n_sync = sync_chain[STAGES - 1];
+    assign x_sync = sync_chain[STAGES - 1];
 
 endmodule
--- mimas_a7/test/uart/rtl/uart.sv
+++ mimas_a7/test/uart/rtl/uart.sv
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module uart(
+module Uart(
     input wire logic reset,
     input wire logic clk,
 
@@ -10,10 +10,13 @@ module uart(
     output wire logic led);
 
     logic reset_n;
-    reset_synchronizer reset_synchronizer0(
+    SyncChain #(.DEFAULT(1'b0)) reset_sync_chain(
         .reset_n(~reset),
         .clk(clk),
-        .reset_n_sync(reset_n));
+
+        .x(1'b1),
+
+        .x_sync(reset_n));
 
     logic [7:0] write_data;
     logic write_req;
@@ -35,18 +38,14 @@ module uart(
         .value(write_data),
         .shift_enable(ready));
 
-    parameter RX_SYNC_STAGES = 2;
+    logic rx_sync;
+    SyncChain #(.DEFAULT(1'b1)) rx_sync_chain(
+        .reset_n(reset_n),
+        .clk(clk),
 
-    (* ASYNC_REG = "TRUE" *) logic [RX_SYNC_STAGES - 1:0] rx_sync_chain;
+        .x(rx),
 
-    always_ff @(posedge clk, negedge reset_n) begin
-        if (~reset_n) begin
-            rx_sync_chain <= {RX_SYNC_STAGES{1'd1}};
-        end
-        else begin
-            rx_sync_chain <= {rx_sync_chain[RX_SYNC_STAGES - 2:0], rx};
-        end
-    end
+        .x_sync(rx_sync));
 
     logic [7:0] read_data;
     logic read_data_ready;
@@ -54,7 +53,7 @@ module uart(
         .reset_n(reset_n),
         .clk(clk),
 
-        .rx(rx_sync_chain[RX_SYNC_STAGES - 1]),
+        .rx(rx_sync),
 
         .data(read_data),
         .data_ready(read_data_ready));
