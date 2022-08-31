af647dab6c8a2c5ba1029e7a59f1847c2f3a2ad3
ferris <yupferris@gmail.com>
2022-08-31 19:54:18 +0200

    Linear device mem allocator for SimDevice
    This is what I had originally intended to implement, but did something else for some reason. And yes, linear sucks, but this will run on the host system and we won't be allocating very much anyways.

--- sw/model-test/src/sim_device.rs
+++ sw/model-test/src/sim_device.rs
@@ -44,14 +44,28 @@ impl SimDevice {
 
 impl Device for SimDevice {
     fn mem_alloc(&mut self, num_words: u32, align_words: u32) -> u32 {
+        let size = num_words * 16;
         let align = align_words * 16;
-        let start = (self.allocations.last().map_or(0, |a| a.end()) + align - 1) / align * align;
-        if start >= MEM_NUM_BYTES {
+        let mut start = 0;
+        let mut end = start + size;
+        let mut insert_index = 0;
+        for (i, allocation) in self.allocations.iter().enumerate() {
+            let allocation_end = allocation.end();
+            if (start >= allocation.start && start < allocation_end) ||
+                (end >= allocation.start && end < allocation_end) ||
+                (allocation.start >= start && allocation.start < end) ||
+                (allocation_end >= start && allocation_end < end) {
+                start = (allocation_end + align - 1) / align * align;
+                end = start + size;
+                insert_index = i + 1;
+            }
+        }
+        if end >= MEM_NUM_BYTES {
             panic!("Out of device memory");
         }
-        self.allocations.push(Allocation {
+        self.allocations.insert(insert_index, Allocation {
             start,
-            size: num_words * 16,
+            size,
         });
         start
     }
