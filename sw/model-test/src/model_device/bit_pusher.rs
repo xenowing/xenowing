5b944ed31befa061bfa7aa369e17ef6c6da2a2c7
ferris <yupferris@gmail.com>
2022-09-09 22:23:24 +0200

    BitPusher: Model AddrUnit module
    Simple enough, but having it explicit in the model like this will help me implement the RTL.

--- sw/model-test/src/model_device/bit_pusher.rs
+++ sw/model-test/src/model_device/bit_pusher.rs
@@ -2,22 +2,12 @@ use crate::model_device::color_thrust::*;
 
 use rtl_meta::bit_pusher::*;
 
-enum Direction {
-    Mem2Sys,
-    Sys2Mem,
-}
-
 pub struct BitPusher {
     direction: Direction,
     num_words: u32,
 
-    sys_addr: u32,
-    sys_words_per_span: u32,
-    sys_span_stride: u32,
-
-    mem_addr: u32,
-    mem_words_per_span: u32,
-    mem_span_stride: u32,
+    sys_addr_unit: AddrUnit,
+    mem_addr_unit: AddrUnit,
 }
 
 impl BitPusher {
@@ -26,13 +16,8 @@ impl BitPusher {
             direction: Direction::Mem2Sys,
             num_words: 0,
 
-            sys_addr: 0,
-            sys_words_per_span: 0,
-            sys_span_stride: 0,
-
-            mem_addr: 0,
-            mem_words_per_span: 0,
-            mem_span_stride: 0,
+            sys_addr_unit: AddrUnit::new(),
+            mem_addr_unit: AddrUnit::new(),
         }
     }
 
@@ -50,22 +35,22 @@ impl BitPusher {
                 self.num_words = data;
             }
             REG_SYS_ADDR_ADDR => {
-                self.sys_addr = data >> 4;
+                self.sys_addr_unit.addr = data >> 4;
             }
             REG_SYS_WORDS_PER_SPAN_ADDR => {
-                self.sys_words_per_span = data;
+                self.sys_addr_unit.words_per_span = data;
             }
             REG_SYS_SPAN_STRIDE_ADDR => {
-                self.sys_span_stride = data;
+                self.sys_addr_unit.span_stride = data;
             }
             REG_MEM_ADDR_ADDR => {
-                self.mem_addr = data >> 4;
+                self.mem_addr_unit.addr = data >> 4;
             }
             REG_MEM_WORDS_PER_SPAN_ADDR => {
-                self.mem_words_per_span = data;
+                self.mem_addr_unit.words_per_span = data;
             }
             REG_MEM_SPAN_STRIDE_ADDR => {
-                self.mem_span_stride = data;
+                self.mem_addr_unit.span_stride = data;
             }
             _ => panic!("Unrecognized addr: {}", addr)
         }
@@ -80,68 +65,94 @@ impl BitPusher {
                 Direction::Sys2Mem => REG_DIRECTION_SYS2MEM,
             }
             REG_NUM_WORDS_ADDR => self.num_words,
-            REG_SYS_ADDR_ADDR => self.sys_addr << 4,
-            REG_SYS_WORDS_PER_SPAN_ADDR => self.sys_words_per_span,
-            REG_SYS_SPAN_STRIDE_ADDR => self.sys_span_stride,
-            REG_MEM_ADDR_ADDR => self.mem_addr << 4,
-            REG_MEM_WORDS_PER_SPAN_ADDR => self.mem_words_per_span,
-            REG_MEM_SPAN_STRIDE_ADDR => self.mem_span_stride,
+            REG_SYS_ADDR_ADDR => self.sys_addr_unit.addr << 4,
+            REG_SYS_WORDS_PER_SPAN_ADDR => self.sys_addr_unit.words_per_span,
+            REG_SYS_SPAN_STRIDE_ADDR => self.sys_addr_unit.span_stride,
+            REG_MEM_ADDR_ADDR => self.mem_addr_unit.addr << 4,
+            REG_MEM_WORDS_PER_SPAN_ADDR => self.mem_addr_unit.words_per_span,
+            REG_MEM_SPAN_STRIDE_ADDR => self.mem_addr_unit.span_stride,
             _ => panic!("Unrecognized addr: {}", addr)
         }
     }
 
     fn transfer(&mut self, mem: &mut [u128], color_thrust: &mut ColorThrust) {
-        let mut sys_span_base = self.sys_addr;
-        let mut sys_span_word_counter = 0;
-
-        let mut mem_span_base = self.mem_addr;
-        let mut mem_span_word_counter = 0;
+        self.sys_addr_unit.start_transfer();
+        self.mem_addr_unit.start_transfer();
 
         loop {
             // TODO: This is ugly and can probably be done in a more unified way with the rest of the code..
-            let sys_base = (self.sys_addr >> 20) & 0x0f; // TODO: Proper constant(s)
-            let sys_offset = self.sys_addr & ((1 << 20) - 1); // TODO: Proper constant(s)
+            let sys_base = (self.sys_addr_unit.addr >> 20) & 0x0f; // TODO: Proper constant(s)
+            let sys_offset = self.sys_addr_unit.addr & ((1 << 20) - 1); // TODO: Proper constant(s)
             match self.direction {
                 Direction::Mem2Sys => {
-                    let word = mem[self.mem_addr as usize];
+                    let word = mem[self.mem_addr_unit.addr as usize];
 
                     match sys_base {
                         0x04 => color_thrust.write_color_buffer_word(sys_offset, word), // TODO: Proper constant
                         0x05 => color_thrust.write_depth_buffer_word(sys_offset, word), // TODO: Proper constant
-                        _ => panic!("Unrecognized sys addr: 0x{:08x}", self.sys_addr << 4)
+                        _ => panic!("Unrecognized sys addr: 0x{:08x}", self.sys_addr_unit.addr << 4)
                     }
                 }
                 Direction::Sys2Mem => {
                     let word = match sys_base {
                         0x04 => color_thrust.read_color_buffer_word(sys_offset), // TODO: Proper constant
                         0x05 => color_thrust.read_depth_buffer_word(sys_offset), // TODO: Proper constant
-                        _ => panic!("Unrecognized sys addr: 0x{:08x}", self.sys_addr << 4)
+                        _ => panic!("Unrecognized sys addr: 0x{:08x}", self.sys_addr_unit.addr << 4)
                     };
 
-                    mem[self.mem_addr as usize] = word;
+                    mem[self.mem_addr_unit.addr as usize] = word;
                 }
             }
 
-            self.sys_addr += 1;
-            sys_span_word_counter += 1;
-            if sys_span_word_counter == self.sys_words_per_span {
-                sys_span_base += self.sys_span_stride;
-                self.sys_addr = sys_span_base;
-                sys_span_word_counter = 0;
-            }
-
-            self.mem_addr += 1;
-            mem_span_word_counter += 1;
-            if mem_span_word_counter == self.mem_words_per_span {
-                mem_span_base += self.mem_span_stride;
-                self.mem_addr = mem_span_base;
-                mem_span_word_counter = 0;
-            }
+            self.sys_addr_unit.step();
+            self.mem_addr_unit.step();
 
             self.num_words -= 1;
             if self.num_words == 0 {
                 break;
             }
+        }
+    }
+}
+
+enum Direction {
+    Mem2Sys,
+    Sys2Mem,
+}
+
+struct AddrUnit {
+    addr: u32,
+    words_per_span: u32,
+    span_stride: u32,
+
+    span_base: u32,
+    span_word_counter: u32,
+}
+
+impl AddrUnit {
+    fn new() -> AddrUnit {
+        AddrUnit {
+            addr: 0,
+            words_per_span: 0,
+            span_stride: 0,
+
+            span_base: 0,
+            span_word_counter: 0,
+        }
+    }
+
+    fn start_transfer(&mut self) {
+        self.span_base = self.addr;
+        self.span_word_counter = 0;
+    }
+
+    fn step(&mut self) {
+        self.addr += 1;
+        self.span_word_counter += 1;
+        if self.span_word_counter == self.words_per_span {
+            self.span_base += self.span_stride;
+            self.addr = self.span_base;
+            self.span_word_counter = 0;
         }
     }
 }
